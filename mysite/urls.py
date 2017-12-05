5ecda0c0bf82e0f68bb1ec8277276d4489c0e242
Simon Zheng <583621555@qq.com>
2017-12-05 17:05:30 +0800

    login without password

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from polls.admin import admin_site
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin_site.urls),
 ]
--- polls/admin.py
+++ polls/admin.py
@@ -1,21 +1,58 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Question, Choice
+
+
+class MyAdminSite(admin.AdminSite):
+    '''
+    custom admin site
+    '''
+    site_header = 'Interstellar based Starfleet'
+
+    def quicklogin(self,request):
+        '''
+        custom view
+        login without authentication
+        :param request:
+        :return:
+        '''
+        from django.contrib.auth import login
+        from django.contrib.auth.models import User
+        user = User.objects.get(id=1)
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(request, user)
+        return HttpResponseRedirect('/admin')
+
+    def get_urls(self):
+        '''
+        add custom view url to urls
+        :return:
+        '''
+        from django.conf.urls import url
+        urls = super(MyAdminSite, self).get_urls()
+        urls += [
+           url(r'^quicklogin/', self.quicklogin, name='quicklogin'),
+        ]
+        return urls
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra =3
+    extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'],
-        'classes' : ['collapse']}),
+        (None, {'fields': ['question_text'], 'description':'content of the question'}),
+        ('Date information', {'fields': ['pub_date'],'classes' : ['collapse']}),
     ]
     inlines= [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
-    search_fields = ['question_text']
+    search_fields = ['question_text', 'pub_date']
+    date_hierarchy = 'pub_date'
+    list_per_page = 10
 
-admin.site.register(Question, QuestionAdmin)
+admin_site = MyAdminSite(name='myadmin')
+admin_site.register(Question, QuestionAdmin)
